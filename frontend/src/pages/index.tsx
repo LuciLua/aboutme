bbaa9be33ac37d7f4639b7cc135dc4a5473cf564
LuciLua <luci.lua81@gmail.com>
2022-07-10 02:41:58 -0300

    🦚 Spline obj add

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -6,6 +6,9 @@ function Home() {
         <div className="home">
             <Card />
             <LinksContainer />
+            <div className="box">
+                <iframe src='https://my.spline.design/untitled-1b069e49f901b45893e3a276df5a45d6/' frameBorder='0' width='100%' height='100%'></iframe>
+            </div>
         </div>
     )
 }
